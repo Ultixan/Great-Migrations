afaf0f2c9f86477a5ac2da60f61222b547f386b6
Ultixan <ultixan@gmail.com>
2011-01-29 00:04:31 +0200

    Basic player selection and race choices are available. They don't do much yet but I'm remembering how javascript works.

---
+++ src/gm.js
@@ -0,0 +1,7 @@
+var Races;
+
+function PickRaces() {
+	var numplayers = document.getElementById("players");
+	document.getElementById("start").style.display = "none";
+	document.getElementById("races").style.display = "block";
+}
--- src/gm.php
+++ src/gm.php
@@ -1,3 +1,29 @@
 <html>
-
+	<head>
+		<link href="gm.css" rel="stylesheet" type="text/css">
+		<script src="gm.js"></script>
+	</head>
+	<body>
+		<div id="start">
+			Please select the number of players:
+			<select id="players">
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+				<option value="5">5</option>
+				<option value="6">6</option>
+			</select>
+			<br/>
+			<button type="button" id="setplayers" onclick="PickRaces();">Next</button>
+		</div>
+		<div id="races">
+			Please select which races are being used:
+			<input type="checkbox" name="race" value="1">Dodo</input>
+			<input type="checkbox" name="race" value="2">Elephant Bird</input>
+			<input type="checkbox" name="race" value="4">Lesser Bilby</input>
+			<input type="checkbox" name="race" value="8">Irish Elk</input>
+			<input type="checkbox" name="race" value="16">Mammoth</input>
+			<input type="checkbox" name="race" value="32">Quagga</input>
+		</div>
+	</body>
 </html>
