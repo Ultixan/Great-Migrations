57ff8b2b2f2bea83c77dc4f9f4d8e06a4caf4891
Ultixan <ultixan@gmail.com>
2011-01-29 02:07:10 +0200

    Have race selection working.

--- src/gm.js
+++ src/gm.js
@@ -1,7 +1,25 @@
-var Races;
+var selectRaces;
+var nextTurn;
 
-function PickRaces() {
-	var numplayers = document.getElementById("players");
+function PickPlayers() {
+	var numplayers = document.getElementById("players").value;
 	document.getElementById("start").style.display = "none";
 	document.getElementById("races").style.display = "block";
+	selectraces = function() {
+		var races = new Array();
+		var boxes = document.getElementById("race_selection").race;
+		for(var i = 0; i < boxes.length; ++i) {
+			if(boxes[i].checked)
+				races.push(boxes[i].value);
+		}
+		if(races.length != numplayers-1) {
+			alert("Please select " + numplayers + " races.");
+		}
+		else {
+			document.getElementById("races").style.display = "none";
+			var game = document.getElementById("game");
+			game.style.display = "block";
+			game.setAttribute("class",races[0]);
+		}
+	}
 }
--- src/gm.php
+++ src/gm.php
@@ -14,16 +14,23 @@
 				<option value="6">6</option>
 			</select>
 			<br/>
-			<button type="button" id="setplayers" onclick="PickRaces();">Next</button>
+			<button type="button" id="setplayers" onclick="PickPlayers();">Next</button>
 		</div>
 		<div id="races">
 			Please select which races are being used:
-			<input type="checkbox" name="race" value="1">Dodo</input>
-			<input type="checkbox" name="race" value="2">Elephant Bird</input>
-			<input type="checkbox" name="race" value="4">Lesser Bilby</input>
-			<input type="checkbox" name="race" value="8">Irish Elk</input>
-			<input type="checkbox" name="race" value="16">Mammoth</input>
-			<input type="checkbox" name="race" value="32">Quagga</input>
+			<form id="race_selection">
+				<input type="checkbox" name="race" value="dodo">Dodo</input>
+				<input type="checkbox" name="race" value="elephant_bird">Elephant Bird</input>
+				<input type="checkbox" name="race" value="lesser_bilby">Lesser Bilby</input>
+				<input type="checkbox" name="race" value="irish_elk">Irish Elk</input>
+				<input type="checkbox" name="race" value="mammoth">Mammoth</input>
+				<input type="checkbox" name="race" value="quagga">Quagga</input>
+				<br/>
+				<button type="button" id="setraces" onclick="selectraces();">Next</button>
+			</form>
+		</div>
+		<div id="game">
+		
 		</div>
 	</body>
 </html>
